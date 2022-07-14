7edd4d79881028dff7494ec764de9663eae31040
Yoh <yokeus2000@gmail.com>
2022-07-14 17:50:09 +0900

    add: Item表示機能と表示ページを追加

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,36 +1,77 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:intern_subscription/pages/sign_in_page.dart';
+import 'package:intern_subscription/date_time_extension.dart';
+import 'package:intern_subscription/providers/item_provider.dart';
 
 class HomePage extends ConsumerWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final auth = FirebaseAuth.instance;
+    final itemStream = ref.watch(itemStreamProvider);
 
     return Scaffold(
         backgroundColor: Colors.grey[300],
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Text('Home'),
-              IconButton(
-                icon: const Icon(Icons.logout),
-                onPressed: () async {
-                  await auth.signOut();
-
-                  Navigator.of(context).push(MaterialPageRoute(
-                    builder: (context) {
-                      return const SignInPage();
-                    },
-                  ));
-                },
-              ),
-            ],
-          ),
-        ));
+        body: itemStream.when(
+            loading: () => const CircularProgressIndicator(),
+            error: (error, stack) => Text('Error: $error'),
+            data: (items) {
+              return Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: ListView.builder(
+                    itemCount: items.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      return Card(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(16),
+                        ),
+                        elevation: 4,
+                        child: SizedBox(
+                            height: 80,
+                            child: Padding(
+                              padding: const EdgeInsets.all(16.0),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    items[index].name,
+                                    style: TextStyle(
+                                        color: Colors.cyan[900],
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 20),
+                                  ),
+                                  const SizedBox(
+                                    height: 6,
+                                  ),
+                                  Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        Row(
+                                          children: [
+                                            Text('¥${items[index].price}',
+                                                style: TextStyle(
+                                                  color: Colors.cyan[900],
+                                                  fontWeight: FontWeight.bold,
+                                                )),
+                                          ],
+                                        ),
+                                        Text(
+                                            items[index]
+                                                .billingAt
+                                                .toFormattedString(
+                                                    'yyyy/MM//dd'),
+                                            style: TextStyle(
+                                              color: Colors.cyan[900],
+                                              fontWeight: FontWeight.bold,
+                                            ))
+                                      ]),
+                                ],
+                              ),
+                            )),
+                      );
+                    }),
+              );
+            }));
   }
 }
--- lib/providers/item_provider.dart
+++ lib/providers/item_provider.dart
@@ -1,7 +1,10 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:intern_subscription/models/item.dart';
 
+// Firestoreに追加するItem情報の状態管理
 // アイテム名
 final nameStateProvider =
     StateProvider.autoDispose((ref) => TextEditingController(text: ''));
@@ -15,3 +18,17 @@ final billingIntervalProvider =
 final startAtProvider = StateProvider<DateTime>((ref) => DateTime.now());
 // 支払日
 final billingAtProvider = StateProvider<DateTime>((ref) => DateTime.now());
+
+// Firestoreから読み込んだItem情報の状態管理
+final itemStreamProvider = StreamProvider.autoDispose((_) {
+  final auth = FirebaseAuth.instance;
+  final snapshots = FirebaseFirestore.instance
+      .collection('users')
+      .doc(auth.currentUser?.uid)
+      .collection('subscription')
+      .orderBy('createdAt', descending: true)
+      .snapshots();
+
+  return snapshots.map((snapshot) =>
+      snapshot.docs.map((doc) => Item.fromJson(doc.data())).toList());
+});
